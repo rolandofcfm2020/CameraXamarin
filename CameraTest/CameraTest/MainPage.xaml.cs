2954c7603faf0ee34b1f9c694dde9f1f59e9699a
Rolando <rolando.fcfm@gmail.com>
2020-06-16 01:11:31 -0500

    first commit

---
+++ CameraTest/CameraTest/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CameraTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //https://stackoverflow.com/questions/45633192/attempt-to-invoke-virtual-method-android-content-res-xmlresourceparser-android
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsTakePhotoSupported && !CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Mensaje", "El teléfono no tiene funcionalidad para tomar fotos", "OK");
+                return;
+            }
+            else
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Directory = "Images",
+                    Name = DateTime.Now+"_test.jpg",
+                    
+                }) ;
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("File Path", file.Path, "OK");
+
+                MyImgage.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+            }
+        }
+    }
+}
